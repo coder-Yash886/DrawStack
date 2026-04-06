81b56c32eedc5ece3344ea5ea54efd16825218e9
coder-Yash886 <yashkumar.967565@gmail.com>
2026-04-06 17:29:08 +0530

    Add the signup signin endpoint

---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+export function middleware(req: Request, res: Response, next: NextFunction){
+    const token = req.headers["authorization"] ?? "";
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+
+        if(decoded.userId){
+            (req as any).userId = decoded.userId;
+            next();
+        } else {
+            res.status(403).json({
+                message: "Unauthorized"
+            });
+        }
+    } catch (err) {
+        res.status(403).json({
+            message: "Invalid token"
+        });
+    }
+}
