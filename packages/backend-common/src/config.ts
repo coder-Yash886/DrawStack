26dd9b45d3ab239e936f06a428c80178bbbf75a7
coder-Yash886 <yashkumar.967565@gmail.com>
2026-04-09 22:51:36 +0530

    Add the common zod validation and db

--- packages/backend-common/src/config.ts
+++ packages/backend-common/src/config.ts
@@ -1,1 +1,11 @@
+import { z } from "zod";
+
+export const JwtConfig = z.object({
+  JWT_SECRET: z.string().min(1)
+});
+
+export function validateEnv(env: unknown) {
+  return JwtConfig.parse(env);
+}
+
 export const JWT_SECRET = process.env.JWT_SECRET || "123123"
---
+++ packages/common/src/schemas.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+
+export const CreateUserSchema = z.object({
+  username: z.string().min(3).max(20),
+  password: z.string().min(6),
+  name: z.string(),
+});
+
+export const SigninSchema = z.object({
+  username: z.string().min(3).max(20),
+  password: z.string(),
+});
+
+
+export const CreateRoomSchema = z.object({
+  name: z.string().min(3).max(20),
+});
+
+
+export type CreateUserInput = z.infer<typeof CreateUserSchema>;
+export type SigninInput = z.infer<typeof SigninSchema>;
+export type CreateRoomInput = z.infer<typeof CreateRoomSchema>;
