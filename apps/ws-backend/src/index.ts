6241c8942813153fa7fd2a99ac297de5b8c32333
coder-Yash886 <yashkumar.967565@gmail.com>
2026-04-14 23:59:25 +0530

    Add the websocket server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,22 @@
 import {WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common";
+import { de } from "zod/v4/locales";
+import { string } from "zod/v4";
 
 const wss = new WebSocketServer({ port: 8000 });
+
+function checkUser(token: string): string | null{
+    const decoded = jwt.verify(token,JWT_SECRET);
+    if(typeof decoded == "string"){
+        return null;
+    }
+
+    if(!decoded || !decoded.userId){
+        return null;
+    }
+    return decoded.userId;
+}
 
 wss.on('connection', function connection(ws, request){
 
@@ -10,19 +24,19 @@ wss.on('connection', function connection(ws, request){
     if(!url){
         return;
     }
-
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token') || "";
     const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    const userId = checkUser(token);
 
-    
+    if(!userId){
+        ws.close();
+    }
 
     if(!decoded || !decoded.userId){
         ws.close(); 
         return;
     }
-
-    
     ws.on('message', function message(data){
         ws.send('pong')
     })
